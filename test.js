2a57e462abeb7b6cb77d801339808307ea418582
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-06-24 08:00:41 -0400

    Add basic testing

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-const fetchBDFMFeed = require('./index');
+const { createClient } = require('./index');
+require('dotenv').config();
 
-const x = fetchBDFMFeed('9c53a03b8da895b96df75e8fb6e9ffd5');
 
+try {
+  const client = createClient(process.env.MTA_API_KEY);
+  client.arrivals({ lines: 'L', stations: '1 Av' })
+    .then((arrivals) => {
+      if (typeof arrivals[0] !== 'object') {
+        throw new Error(`arrivals should contain object, found: \n${JSON.stringify(arrivals[0], null, 2)}`);
+      }
+    });
+} catch (e) {
+  throw e;
+}
+
+console.log('Tests complete');
