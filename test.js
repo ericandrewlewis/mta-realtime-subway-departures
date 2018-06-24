269c6f4934ee0ab1de80d4b38a3eae914c7f0bb8
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-06-24 08:05:57 -0400

    Add test for stations

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
 const { createClient } = require('./index');
 require('dotenv').config();
 
+const client = createClient(process.env.MTA_API_KEY);
+client.arrivals({ lines: 'L', stations: '1 Av' })
+  .then((arrivals) => {
+    if (typeof arrivals[0] !== 'object') {
+      throw new Error(`arrivals should contain object, found: \n${JSON.stringify(arrivals[0], null, 2)}`);
+    }
+  });
 
-try {
-  const client = createClient(process.env.MTA_API_KEY);
-  client.arrivals({ lines: 'L', stations: '1 Av' })
-    .then((arrivals) => {
-      if (typeof arrivals[0] !== 'object') {
-        throw new Error(`arrivals should contain object, found: \n${JSON.stringify(arrivals[0], null, 2)}`);
-      }
-    });
-} catch (e) {
-  throw e;
+const { stations } = require('./index');
+
+if (typeof stations[0]["Station ID"] !== 'string') {
+  throw new Error('stations should contain an object with a `Station ID` property');
 }
 
 console.log('Tests complete');
