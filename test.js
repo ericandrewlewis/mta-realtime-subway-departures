a2aaf38f7bfa02f501589d30bd0d6d48629ad8bf
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2020-07-15 09:46:51 -0400

    Refactor test file.
    It won't say "All tests pass" when they actually don't, and captures
    promise errors to provide less noise when an error is found.

--- test.js
+++ test.js
@@ -3,20 +3,21 @@ require('dotenv').config();
 
 const client = createClient(process.env.MTA_API_KEY);
 
-// West 4th street
-client.departures(167)
-  .then((response) => {
+(async () => {
+  try {
+    let response = await client.departures(167);
     if (response.lines[0].departures.N.length < 1) {
       throw new Error(`There should be departures, none found: \n${JSON.stringify(response, null, 2)}`);
     }
-  });
-
-  // Multiple complex IDs
-client.departures([167, 232])
-  .then((responses) => {
+    let responses = await client.departures([167, 232])
     if (responses.length !== 2) {
       throw new Error(`There should be two responses: \n${JSON.stringify(responses, null, 2)}`);
     }
-  });
+  } catch(e) {
+    console.error(e);
+    console.log('Did you set the MTA_API_KEY from https://api.mta.info ?')
+    return;
+  }
 
-console.log('💁‍♀️ All tests pass.');
+  console.log('💁‍♀️ All tests pass.');
+})();
